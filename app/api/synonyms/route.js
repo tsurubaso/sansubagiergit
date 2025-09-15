40cd6de22f2834277824c861b82621e546ff3d20
tsurubaso <tsurubaso@hotmail.com>
2025-09-15 10:50:36 +0900

    working like a charm sunonyms and dico

--- app/api/synonyms/route.js
+++ app/api/synonyms/route.js
@@ -3,25 +3,38 @@
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const { searchParams } = new URL(req.url);
-  const word = searchParams.get("word");
-
-  // dictionary
-  if (!word) {
-    return NextResponse.json({ error: "No word provided" }, { status: 400 });
-  }
-
-  const frdicokey = process.env.FRDICOKEY;
   try {
-    
+    const { searchParams } = new URL(req.url);
+    const word = searchParams.get("word");
+    const frdicokey = process.env.FRDICOKEY;
+
+    if (!word) {
+      return NextResponse.json({ error: "No word provided" }, { status: 400 });
+    }
+
     const res = await fetch(
       `https://api.dicolink.com/v1/mot/${encodeURIComponent(
         word
       )}/synonymes?limite=200&api_key=${frdicokey}`
     );
+
+        if (!res.ok) {
+      return NextResponse.json(
+        { error: "Erreur Dicolink" },
+        { status: response.status }
+      );
+    }
     const data = await res.json();
-    res.status(200).json(data);
+    // ⚠️ Ici pas de "definition", juste mot + URL
+    const formatted = data.map((entry) => ({
+      mot: entry.mot,
+      dicolinkUrl: entry.dicolinkUrl,
+    }));
+
+    return NextResponse.json(formatted);
+
+
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
